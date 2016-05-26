a0c2605e6a80ef3a2ef6af33e66373a1bd8f181c
NumberI <x0987654321@ya.ru>
2016-05-26 17:48:26 +0300

    init commit

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home =React.createClass({
+  render: function () {
+    return (
+      <div> Hello from hell!!! </div>
+    )
+  }
+});
+
+module.exports = Home;
