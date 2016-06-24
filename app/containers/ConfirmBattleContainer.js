a3dc53c7646b379e2d040ba47117c97e35f0068f
NumberI <x0987654321@ya.ru>
2016-06-24 17:54:15 +0300

    add async/await

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -18,16 +18,18 @@ const ConfirmBattleContainer = React.createClass({
   componentWillMount () {
     console.log('componentWillMount');
   },
-  componentDidMount () {
+  async componentDidMount () {
     console.log('componentDidMount');
     const { query } = this.props.location;
-    getPlayersInfo([query.playerOne, query.playerTwo])
-      .then((players) => {
-        this.setState({
-          isLoading: false,
-          playersInfo: [players[0], players[1]]
-        }) 
+    try {
+      const players = await getPlayersInfo([query.playerOne, query.playerTwo])
+      this.setState({
+        isLoading: false,
+        playersInfo: [players[0], players[1]]
       })
+    } catch (error) {
+      console.warn('Error in ConfirmBattleContainer:', error)
+    } 
   },
   handleInitiateBattle () {
     this.context.router.push({
